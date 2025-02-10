38ccbc20f6fc63fdcd09a4f409c35d31a4379090
Mark1-15 <markmarsden04@gmail.com>
2025-02-10 16:05:16 +0000

    added first post method in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,24 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+}
 
 async function getStatus(e) {
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -16,6 +32,7 @@ async function getStatus(e) {
     } else {
         throw new Error(data.error);
     }
+
 }
 
 function displayStatus(data) {
@@ -24,7 +41,8 @@ function displayStatus(data) {
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
 
-    document.getElementById(resultsModalTitle).innerText = heading;
-    document.getElementById(results-content).innerHTML = results;
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+
 }
